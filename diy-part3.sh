41da7a981fad4be8aeeeed85f3a862e897c353ff
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-04-02 21:10:32 +0800

    Add files via upload

---
+++ diy-part3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#===============================================
+# Description: DIY script
+# File name: diy-script.sh
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#===============================================
+
+# 移植黑豹x2
+
+# rm -f target/linux/rockchip/image/rk35xx.mk
+# cp -f $GITHUB_WORKSPACE/file/rk35xx.mk target/linux/rockchip/image/rk35xx.mk
+
+
+# rm -f target/linux/rockchip/rk35xx/base-files/lib/board/init.sh
+# cp -f $GITHUB_WORKSPACE/file/init.sh target/linux/rockchip/rk35xx/base-files/lib/board/init.sh
+
+
+# rm -f target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
+# cp -f $GITHUB_WORKSPACE/file/02_network target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
+
+
+sed -i "s/option\s*script_timeout\s*60/option script_timeout 360/g" package/network/services/uhttpd/files/uhttpd.config
+
+
+
+
+# 修改内核配置文件
+# rm -f target/linux/rockchip/rk35xx/config-5.10
+# cp -f $GITHUB_WORKSPACE/file/config-5.10 target/linux/rockchip/rk35xx/config-5.10
+sed -i "/# CONFIG_ROCKCHIP_RGA2 is not set/d" target/linux/rockchip/rk35xx/config-5.10
+sed -i "/CONFIG_ROCKCHIP_RGA2_DEBUGGER=y/d" target/linux/rockchip/rk35xx/config-5.10
+sed -i "/CONFIG_ROCKCHIP_RGA2_DEBUG_FS=y/d" target/linux/rockchip/rk35xx/config-5.10
+sed -i "/CONFIG_ROCKCHIP_RGA2_PROC_FS=y/d" target/linux/rockchip/rk35xx/config-5.10
+
+
+
+
+# 替换dts文件
+cp -f $GITHUB_WORKSPACE/file/rk3566-jp-tvbox.dts target/linux/rockchip/dts/rk3568/rk3566-jp-tvbox.dts
+
+cp -f $GITHUB_WORKSPACE/file/rk3566-panther-x2.dts target/linux/rockchip/dts/rk3568/rk3566-panther-x2.dts
