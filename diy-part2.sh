9df6984c5531bec41ac71b5255cd422de8373a51
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-04-14 00:49:26 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,15 +10,15 @@
 # 移植黑豹x2
 
 # rm -f target/linux/rockchip/image/rk35xx.mk
-# cp -f $GITHUB_WORKSPACE/file/rk35xx.mk target/linux/rockchip/image/rk35xx.mk
+# cp -f $GITHUB_WORKSPACE/files/rk35xx.mk target/linux/rockchip/image/rk35xx.mk
 
 
 # rm -f target/linux/rockchip/rk35xx/base-files/lib/board/init.sh
-# cp -f $GITHUB_WORKSPACE/file/init.sh target/linux/rockchip/rk35xx/base-files/lib/board/init.sh
+# cp -f $GITHUB_WORKSPACE/files/init.sh target/linux/rockchip/rk35xx/base-files/lib/board/init.sh
 
 
 # rm -f target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
-# cp -f $GITHUB_WORKSPACE/file/02_network target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
+# cp -f $GITHUB_WORKSPACE/files/02_network target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
 
 
 # sed -i "s/option\s*script_timeout\s*60/option script_timeout 360/g" package/network/services/uhttpd/files/uhttpd.config
@@ -28,7 +28,7 @@
 
 # 修改内核配置文件
 # rm -f target/linux/rockchip/rk35xx/config-5.10
-# cp -f $GITHUB_WORKSPACE/file/config-5.10 target/linux/rockchip/rk35xx/config-5.10
+# cp -f $GITHUB_WORKSPACE/files/config-5.10 target/linux/rockchip/rk35xx/config-5.10
 sed -i "/# CONFIG_ROCKCHIP_RGA2 is not set/d" target/linux/rockchip/rk35xx/config-5.10
 sed -i "/CONFIG_ROCKCHIP_RGA2_DEBUGGER=y/d" target/linux/rockchip/rk35xx/config-5.10
 sed -i "/CONFIG_ROCKCHIP_RGA2_DEBUG_FS=y/d" target/linux/rockchip/rk35xx/config-5.10
@@ -38,9 +38,9 @@ sed -i "/CONFIG_ROCKCHIP_RGA2_PROC_FS=y/d" target/linux/rockchip/rk35xx/config-5
 
 
 # 替换dts文件
-cp -f $GITHUB_WORKSPACE/file/rk3566-jp-tvbox.dts target/linux/rockchip/dts/rk3568/rk3566-jp-tvbox.dts
+cp -f $GITHUB_WORKSPACE/files/rk3566-jp-tvbox.dts target/linux/rockchip/dts/rk3568/rk3566-jp-tvbox.dts
 
-cp -f $GITHUB_WORKSPACE/file/rk3566-panther-x2.dts target/linux/rockchip/dts/rk3568/rk3566-panther-x2.dts
+cp -f $GITHUB_WORKSPACE/files/rk3566-panther-x2.dts target/linux/rockchip/dts/rk3568/rk3566-panther-x2.dts
 
 
 
@@ -66,6 +66,11 @@ sed -i "s/panther,x2|\\\/panther,x2|\\\\\n	dg,nas|\\\/g" target/linux/rockchip/r
 sed -i "s/panther,x2|\\\/panther,x2|\\\\\n	dg,nas|\\\/g" target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
 
 
-cp -f $GITHUB_WORKSPACE/file/rk3568-firefly-roc-pc-se-core.dtsi target/linux/rockchip/dts/rk3568/rk3568-firefly-roc-pc-se-core.dtsi
+cp -f $GITHUB_WORKSPACE/files/rk3568-firefly-roc-pc-se-core.dtsi target/linux/rockchip/dts/rk3568/rk3568-firefly-roc-pc-se-core.dtsi
 
-cp -f $GITHUB_WORKSPACE/file/rk3568-firefly-roc-pc-se.dts target/linux/rockchip/dts/rk3568/rk3568-firefly-roc-pc-se.dts
+cp -f $GITHUB_WORKSPACE/files/rk3568-firefly-roc-pc-se.dts target/linux/rockchip/dts/rk3568/rk3568-firefly-roc-pc-se.dts
+
+
+#增加一键补全WiFi脚本进系统里
+cp -f $GITHUB_WORKSPACE/files/brcmfmac43430-sdio-panther-x2.sh package/base-files/files/sbin/brcmfmac43430-sdio-panther-x2.sh
+chmod 755 package/base-files/files/sbin/brcmfmac43430-sdio-panther-x2.sh
