ce898ef277f52455b3614348b2c2163b90577233
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-04-16 17:42:44 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,3 @@
 # 修改版本为编译日期，数字类型。
 date_version=$(date +"%Y%m%d%H")
 echo $date_version > version
-
-# 为iStoreOS固件版本加上编译作者
-author="xiaomeng9597"
-sed -i "s/DISTRIB_DESCRIPTION.*=.*/DISTRIB_DESCRIPTION='%D %V $date_version by $author'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/OPENWRT_RELEASE.*=.*/OPENWRT_RELEASE=\"%D %V $date_version by $author\"/g" package/base-files/files/usr/lib/os-release
