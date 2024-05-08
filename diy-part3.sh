bce51ae38d271eeac07648de21cab462d386b145
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-05-09 00:47:38 +0800

    Add files via upload

--- diy-part3.sh
+++ diy-part3.sh
@@ -57,5 +57,5 @@ cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 
 
 #增加黑豹X2的一键补全WiFi脚本进系统里
-cp -f $GITHUB_WORKSPACE/configfiles/brcmfmac43430-sdio-panther-x2.sh package/base-files/files/sbin/brcmfmac43430-sdio-panther-x2.sh
-chmod 755 package/base-files/files/sbin/brcmfmac43430-sdio-panther-x2.sh
+# cp -f $GITHUB_WORKSPACE/configfiles/brcmfmac43430-sdio-panther-x2.sh package/base-files/files/sbin/brcmfmac43430-sdio-panther-x2.sh
+# chmod 755 package/base-files/files/sbin/brcmfmac43430-sdio-panther-x2.sh
