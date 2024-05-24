c5b475b615d71ac5a173d4307d2178199630d8f1
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-05-24 17:13:03 +0800

    近期istoreos网站文件服务器不稳定，临时增加一个自定义下载网址

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,8 @@
 cp -f $GITHUB_WORKSPACE/configfiles/ubus_Makefile package/system/ubus/Makefile
 
 
-# 增加一个自定义下载网址
-sed -i "s/push @mirrors, 'https:\/\/mirror2.openwrt.org\/sources';/&\\npush @mirrors, 'https:\/\/github.com\/xiaomeng9597\/files\/releases\/download\/linkmount';/g" scripts/download.pl
+# 近期istoreos网站文件服务器不稳定，临时增加一个自定义下载网址
+sed -i "s/push @mirrors, 'https:\/\/mirror2.openwrt.org\/sources';/&\\npush @mirrors, 'https:\/\/github.com\/xiaomeng9597\/files\/releases\/download\/iStoreosFile';/g" scripts/download.pl
 
 
 # 移植黑豹x2
