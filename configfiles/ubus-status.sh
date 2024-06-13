e0409e0bd80b8d0bc5720349fa560cca72b02081
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-06-13 10:12:13 +0800

    update ubus-status.sh

--- configfiles/ubus-status.sh
+++ configfiles/ubus-status.sh
@@ -5,14 +5,16 @@ do
     dbus_status=$(/etc/init.d/dbus status | grep -o "running")
     datetime=$(date +"%Y-%m-%d %H:%M:%S")
     pidv=$(pgrep "ubusd" | head -n 1)
+    pidv2=$(pgrep "rpcd" | head -n 1)
 
-    if [ -n "$dbus_status" ]; then
-        echo "$datetime / Ubus服务正在运行，一切正常。"
-    elif [ -z "$pidv" ]; then
+    if [ -z "$pidv" ]; then
         echo "$datetime / Ubus服务异常，正在重启Ubus。"
         /sbin/ubusd &
-        killall rpcd
+    elif [ -z "$pidv2" ]; then
+        echo "$datetime / Ubus服务异常，正在重启Ubus。"
         /sbin/rpcd -s /var/run/ubus/ubus.sock -t 30 &
+    elif [ -n "$dbus_status" ]; then
+        echo "$datetime / Ubus服务正在运行，一切正常。"
     else
         echo "$datetime / Ubus服务异常，正在重启Ubus。"
         killall ubusd
