c61c9a273ed877d2d3aaf8f864aa3f491ddc2e28
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-07-02 21:13:12 +0800

    update ubus-status.sh

--- configfiles/ubus-status.sh
+++ configfiles/ubus-status.sh
@@ -42,12 +42,6 @@ check_ubus() {
         /sbin/rpcd -s /var/run/ubus/ubus.sock -t 30 &
     elif echo "$rpcd_status" | grep -q "running"; then
         echo "$datetime / Ubus服务正在运行，一切正常。"
-    else
-        echo "$datetime / Ubus服务异常，正在重启Ubus。"
-        killall ubusd 2>/dev/null
-        killall rpcd 2>/dev/null
-        sleep 1
-        /sbin/rpcd -s /var/run/ubus/ubus.sock -t 30 &
     fi
 }
 
