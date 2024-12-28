b62e81b40b133f85c8525b0416bad8c246f7afed
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-12-28 09:07:46 -0600

    删除引起iproute2依赖报错的补丁

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,14 +7,14 @@
 # Blog: https://p3terx.com
 #===============================================
 
+# 删除引起iproute2依赖报错的补丁
+[ -e package/libs/elfutils/patches/999-fix-odd-build-oot-kmod-fail.patch ] && rm -f package/libs/elfutils/patches/999-fix-odd-build-oot-kmod-fail.patch
 
 # update ubus git HEAD
 cp -f $GITHUB_WORKSPACE/configfiles/ubus_Makefile package/system/ubus/Makefile
 
-
 # 近期istoreos网站文件服务器不稳定，临时增加一个自定义下载网址
 sed -i "s/push @mirrors, 'https:\/\/mirror2.openwrt.org\/sources';/&\\npush @mirrors, 'https:\/\/github.com\/xiaomeng9597\/files\/releases\/download\/iStoreosFile';/g" scripts/download.pl
-
 
 
 # 修改内核配置文件
